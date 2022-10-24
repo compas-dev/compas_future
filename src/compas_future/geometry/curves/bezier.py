986961cabdd1345328ac9a6bb8f9b86f721a69a3
brgcode <tom.v.mele@gmail.com>
2022-10-24 21:36:16 +0200

    stubs for bezier and bspline

---
+++ src/compas_future/geometry/curves/bezier.py
@@ -0,0 +1,42 @@
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
+from compas_future.geometry.curves.curve import Curve
+
+
+class BezierCurve(Curve):
+    def __init__(self, *args, **kwargs):
+        super(BezierCurve, self).__init__(*args, **kwargs)
+
+    @property
+    def is_periodic(self):
+        raise NotImplementedError
+
+    @property
+    def is_closed(self):
+        raise NotImplementedError
+
+    @property
+    def domain(self):
+        raise NotImplementedError
+
+    @property
+    def start(self):
+        raise NotImplementedError
+
+    @property
+    def end(self):
+        raise NotImplementedError
+
+    def point_at(self, t):
+        raise NotImplementedError
+
+    def frame_at(self, t):
+        raise NotImplementedError
+
+    def tangent_at(self, t):
+        raise NotImplementedError
+
+    def normal_at(self, t):
+        raise NotImplementedError
